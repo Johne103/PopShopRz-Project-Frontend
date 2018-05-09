971995f8b1beaf303922e061d4eb7981f441c522
John Evans <johne103@hotmail.com>
2018-05-09 21:01:12 +0100

    update product.js tag.js places.js models

---
+++ src/js/models/tag.js
@@ -0,0 +1,9 @@
+angular.module('PopShopRzProject')
+  .factory('Tag', Tag);
+
+Tag.$inject = ['$resource', 'API_URL'];
+function Tag($resource, API_URL) {
+  return new $resource(`${API_URL}/tags/:id`, { id: '@id' }, {
+    update: { method: 'PUT' }
+  });
+}
